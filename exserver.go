eb3bad531c622d4de8666369b6b95288b2e38902
must11 <must11@users.noreply.github.com>
2024-08-22 11:51:04 +0800

    Add files via upload

--- exserver.go
+++ exserver.go
@@ -1,4 +1,4 @@
-package endless
+package exserver
 
 import (
 	"context"
@@ -10,22 +10,26 @@ import (
 	"sync/atomic"
 )
 
+type ErrChan chan error
+
 type ExServer struct {
 	srv         *http.Server
 	running     atomic.Bool
-	CloseSignal []os.Signal
-	beforeRun   []func(es *ExServer)
+	closeSignal []os.Signal
+	onShutdown  []func()
 	inShutdown  atomic.Bool // true when server is in shutdown
 	addr        string
 	handler     http.Handler
 	closeChan   chan struct{}
 	mu          sync.Mutex
+	ErrChan     ErrChan
 }
 
 func NewExServer() *ExServer {
 	return &ExServer{
-		beforeRun:   make([]func(es *ExServer), 0),
-		CloseSignal: make([]os.Signal, 0),
+		onShutdown:  make([]func(), 0),
+		closeSignal: make([]os.Signal, 0),
+		ErrChan:     newErrChan(),
 	}
 }
 
@@ -33,7 +37,9 @@ func (es *ExServer) Running() bool {
 	return es.running.Load()
 }
 
-func (es *ExServer) SyncRun(errChannel chan error) {
+// 异步启动http.srv
+// wg 等待服务运行结束，在启动服务时add(1)，错误和关闭服务Done，如果不需要等待服务运行结束，设为nil
+func (es *ExServer) SyncRun(wg *sync.WaitGroup) {
 	if es.Running() {
 		return
 	}
@@ -44,20 +50,34 @@ func (es *ExServer) SyncRun(errChannel chan error) {
 	}
 	es.closeChan = make(chan struct{})
 	es.srv = &http.Server{Addr: es.addr, Handler: es.handler}
+	for _, f := range es.onShutdown {
+		es.srv.RegisterOnShutdown(f)
+	}
+	if wg != nil {
+		wg.Add(1)
+	}
+	log.Printf("start listen at %s", es.addr)
 	go func() {
+		defer func() {
+			if wg != nil {
+				wg.Done()
+			}
+		}()
 		if err := es.srv.ListenAndServe(); err != http.ErrServerClosed {
 			log.Printf("server listen err: %v", err)
-			errChannel <- err
+			es.ErrChan <- err
 		} else {
-			errChannel <- nil
+			es.ErrChan <- nil
 		}
+		es.running.Store(false)
+		es.inShutdown.Store(true)
 		close(es.closeChan)
 
 	}()
-	if len(es.CloseSignal) > 0 {
+	if len(es.closeSignal) > 0 {
 		go func() {
 			sigint := make(chan os.Signal, 1)
-			signal.Notify(sigint, es.CloseSignal...)
+			signal.Notify(sigint, es.closeSignal...)
 			<-sigint
 			es.Shutdown(context.Background())
 		}()
@@ -67,6 +87,7 @@ func (es *ExServer) SyncRun(errChannel chan error) {
 }
 
 func (es *ExServer) Shutdown(ctx context.Context) error {
+	log.Print("serve runing", es.Running())
 	if !es.Running() || es.srv == nil {
 		log.Printf("server is not running  ")
 		return nil
@@ -79,11 +100,28 @@ func (es *ExServer) Shutdown(ctx context.Context) error {
 	} else {
 		log.Printf("server shutdown message : %v", err)
 		<-es.closeChan
-		es.running.Store(false)
-		es.inShutdown.Store(true)
 		return nil
 	}
 
+}
+func (es *ExServer) AddCloseSignal(signal ...os.Signal) {
+	es.mu.Lock()
+	defer es.mu.Unlock()
+	es.closeSignal = append(es.closeSignal, signal...)
+}
+func (es *ExServer) RemoveCloseSignal(signal os.Signal) {
+	es.mu.Lock()
+	defer es.mu.Unlock()
+	newCloseSignal := make([]os.Signal, 0, len(es.closeSignal))
+	for _, v := range es.closeSignal {
+		if v != signal {
+			newCloseSignal = append(newCloseSignal, v)
+		}
+	}
+	es.closeSignal = newCloseSignal
+}
+func (es *ExServer) CleanCloseSignal() {
+	es.closeSignal = es.closeSignal[:0]
 }
 
 func (es *ExServer) Listen(addr string, handler http.Handler) {
@@ -91,19 +129,29 @@ func (es *ExServer) Listen(addr string, handler http.Handler) {
 	es.handler = handler
 }
 
-func (es *ExServer) ReBoot(ctx context.Context, errChannel chan error) {
+// 调用reboot,
+// errChannel 必须跟之前保持一致
+func (es *ExServer) ReBoot(ctx context.Context, wg *sync.WaitGroup) {
+	log.Println("start reboot server")
 	if !es.running.Load() {
-		es.SyncRun(errChannel)
+		es.SyncRun(wg)
 	} else {
 		es.Shutdown(ctx)
-		if err := <-errChannel; err != nil {
-			errChannel <- err
+		if err := <-es.ErrChan; err != nil {
+			es.ErrChan <- err
 			return
 		}
-		es.SyncRun(errChannel)
+		es.SyncRun(wg)
 	}
 }
 
-func NewErrChan() chan error {
+// 同http.server.RegisterOnShutdown
+func (es *ExServer) RegisterOnShutdown(f func()) {
+	es.mu.Lock()
+	es.onShutdown = append(es.onShutdown, f)
+	es.mu.Unlock()
+}
+
+func newErrChan() ErrChan {
 	return make(chan error, 1)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module com.github.must11.exserver
+
+go 1.23.0
